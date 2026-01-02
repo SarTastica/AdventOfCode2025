c1e52c0cae3fefcc8e09a1fe52a28191f9960d4f
SarTastica <sartastica@gmail.com>
2026-01-02 17:55:04 +0000

    Day02ATest.java class created

--- src/test/java/test/Day02ATest.java
+++ src/test/java/test/Day02ATest.java
@@ -1,4 +1,29 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+import software.aoc.day02.a.GiftShop;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Day02ATest {
+
+    @Test
+    public void solveDay02PartA() throws IOException {
+        Path path = Paths.get("src/test/resources/day02-a/input.txt");
+        String input = Files.readString(path); // Leemos todo el contenido como un solo String
+
+        GiftShop giftShop = new GiftShop();
+        long result = giftShop.calculateInvalidIdSum(input);
+
+        System.out.println("***********************************");
+        System.out.println("SOLUCION DAY 2 - PART A: " + result);
+        System.out.println("***********************************");
+
+        assertTrue(result > 0);
+    }
 }
